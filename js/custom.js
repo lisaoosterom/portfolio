f0edd70944c62e172bf8696e125a1b46d307760f
lisa oosterom <Lisa-oosterom@live.nl>
2019-02-04 15:47:23 +0100

    bestanden/images/css/js

---
+++ js/custom.js
@@ -0,0 +1,265 @@
+/* jQuery Pre loader
+ -----------------------------------------------*/
+$(window).load(function () {
+    $('.preloader').fadeOut(1000); // set duration in brackets
+});
+
+
+/* Google Map
+-----------------------------------------------*/
+var map = '';
+var center;
+
+function initialize() {
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(13.758468, 100.567481),
+        scrollwheel: false
+    };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function () {
+        calculateCenter();
+    });
+
+    google.maps.event.addDomListener(window, 'resize', function () {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+    center = map.getCenter();
+}
+
+function loadGoogleMap() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+$(function () {
+    loadGoogleMap();
+});
+
+
+/* Istope Portfolio
+-----------------------------------------------*/
+jQuery(document).ready(function ($) {
+
+    if ($('.iso-box-wrapper').length > 0) {
+
+        var $container = $('.iso-box-wrapper'),
+            $imgs = $('.iso-box img');
+
+        $container.imagesLoaded(function () {
+
+            $container.isotope({
+                layoutMode: 'fitRows',
+                itemSelector: '.iso-box'
+            });
+
+            $imgs.load(function () {
+                $container.isotope('reLayout');
+            })
+
+        });
+
+        //filter items on button click
+
+        $('.filter-wrapper li a').click(function () {
+
+            var $this = $(this),
+                filterValue = $this.attr('data-filter');
+
+            $container.isotope({
+                filter: filterValue,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
+
+            // don't proceed if already selected
+
+            if ($this.hasClass('selected')) {
+                return false;
+            }
+
+            var filter_wrapper = $this.closest('.filter-wrapper');
+            filter_wrapper.find('.selected').removeClass('selected');
+            $this.addClass('selected');
+
+            return false;
+        });
+
+    }
+
+});
+
+
+/* Filter items tech/ visual / interaction (eigen code)
+ -----------------------------------------------*/
+
+filterSelection("all")
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+
+
+/* Navigation Bar
+ -----------------------------------------------*/
+$(document).ready(function () {
+    "use strict";
+
+    // Navbar Sticky
+
+    (function () {
+        var docElem = document.documentElement,
+            didScroll = false,
+            stickynav = 50;
+        document.querySelector('.nav-container');
+
+        function init() {
+            window.addEventListener('scroll', function () {
+                if (!didScroll) {
+                    didScroll = true;
+                    setTimeout(scrollPage, 50);
+                }
+            }, false);
+        }
+
+        function scrollPage() {
+            var sy = scrollY();
+            if (sy >= stickynav) {
+                $('.nav-container').addClass('sticky');
+            } else {
+                $('.nav-container').removeClass('sticky');
+            }
+            didScroll = false;
+        }
+
+        function scrollY() {
+            return window.pageYOffset || docElem.scrollTop;
+        }
+        init();
+    })();
+
+});
+
+
+$(document).ready(function () {
+
+    "use strict";
+
+    $('.menu-container').each(function (index) {
+        $(this).find('.circle').attr('menu-link', index);
+        $(this).find('.list-menu').clone().appendTo('body').attr('menu-link', index);
+    });
+
+    $('.menu-container .circle').click(function () {
+        var linkedVideo = $('section').closest('body').find('.list-menu[menu-link="' + $(this).attr('menu-link') + '"]');
+        linkedVideo.toggleClass('reveal-modal');
+
+    });
+
+    $('section').closest('body').find('.close-iframe').click(function () {
+        $(this).closest('.list-menu').toggleClass('reveal-modal');
+    });
+
+
+    /* wow
+    -------------------------------*/
+    new WOW({
+        mobile: false
+    }).init();
+
+});
+
+
+/* slide show arduino
+    -------------------------------*/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("UbicompSlide");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+
+
+/* slide show hci
+    -------------------------------*/
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("HciSlide");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}

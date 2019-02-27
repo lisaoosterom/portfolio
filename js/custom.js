139ad8148de72a7dc958215915851182ea804fb4
lisa oosterom <Lisa-oosterom@live.nl>
2019-02-27 11:44:48 +0100

    slideShowFixed

--- js/custom.js
+++ js/custom.js
@@ -239,7 +239,7 @@ function showDivs(n) {
 
 
 
-/* slide show
+/* slide show WEB
     -------------------------------*/
 
 /* Slideshow JavaScript */
@@ -273,10 +273,39 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].classList.add("active");
 }
+
+
+/* slide show Flower Alarm
+    -------------------------------*/
+
 /* Slideshow JavaScript */
+var slideIndex = 1;
+showSlides(slideIndex);
 
-/* verrander img over mij when hoover
------------------------------------------------*/
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-var fotoOvermij = document.querySelector('#OVermijFoto')
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    };
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+}
